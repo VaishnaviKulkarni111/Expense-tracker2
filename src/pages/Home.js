3a466e4431327fa1bd7fc4051cb36a5568463aca
VaishnaviKulkarni111 <vaishnavikulkarni1822@gmail.com>
2024-03-06 12:24:21 +0530

    final

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,9 @@
 import { useNavigate } from "react-router-dom"
 import './home.css'
 import { Button } from "react-bootstrap"
+import { useSelector } from "react-redux";
 function HomePage(){
+    const isLoggedIn = useSelector((state) => state.auth.isAuthenticated);
 const navigate = useNavigate()
 const updateHandler = () =>{
 navigate('/profile')
@@ -10,11 +12,12 @@ navigate('/profile')
 return(<>
 
     <h1 className="h1"> Welcome to Expense Tracker !</h1>
-    <section className="section">
+    {!isLoggedIn && <h4>Please Login to use the provided services </h4>}
+   { isLoggedIn && <section className="section">
     <p>Your profile is incomplete 
 <Button onClick={updateHandler} variant="link"> Complete now</Button>
 </p>
-    </section>
+    </section>}
     </>)
    
 }
